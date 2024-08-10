8db9f066198758d53a13295e2e0fee0332bfaf72
fpereverzev <fpereverzevgit@mail.ru>
2024-08-10 15:18:20 +0300

    commit

--- d6.2_mail_/simpleapp/simpleapp/board/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.views import View
-from .tasks import hello
-
-class IndexView(View):
-    def get(self, request):
-        hello.delay()
-        return HttpResponse('Hello!')
